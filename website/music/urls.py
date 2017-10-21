f930ce25edc0af20ba5c4f0e1d90c548556c074b
Akhil Gupta <akhil9tiet@gmail.com>
2017-10-21 18:01:49 -0400

    Added images and secondary click functionality

--- website/music/urls.py
+++ website/music/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index), #index page
+    url(r'^$', views.index , name='index'), #index page
 
     #/music/XXX/
     url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
--- website/music/views.py
+++ website/music/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -6,9 +7,11 @@ from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    template=loader.get_template('music/index.html')
-    context={'all_albums': all_albums,}
-    return HttpResponse(template.render(context, request))
+    return render(request,'music/index.html',{'all_albums': all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Album Name:"++"</h2>x")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album doesnot exist")
+    return render(request,'music/detail.html',{'album': album})
