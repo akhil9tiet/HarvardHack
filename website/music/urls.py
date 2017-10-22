c56d44ba89060d3a3d7153a2657d5a03b7df4fbf
Akhil Gupta <akhil9tiet@gmail.com>
2017-10-21 23:14:32 -0400

    Adding search without functionality

--- website/music/urls.py
+++ website/music/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from . import views
 
+app_name='music'
 urlpatterns = [
     # /music/
     url(r'^$', views.index , name='index'), #index page
 
     #/music/XXX/
     url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
+
+    #/music/XXX/favorite
+    url(r'^(?P<album_id>[0-9]+)/favorite/$',views.favorite,name='favorite'),
 ]
